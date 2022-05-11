822b1a543bc3add91a26397a734c61bb993e34ec
Harald_Vandoorne <harald.vandoorne@gmail.com>
2022-05-11 10:41:31 +0200

    Added Controllers-Classes-Models

---
+++ AirWaze/Controllers/AirlineController.cs
@@ -0,0 +1,53 @@
+﻿using AirWaze.Entities;
+using AirWaze.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AirWaze.Controllers
+{
+    public class AirlineController : Controller
+    {
+        private static List<Airline> airlineEntities = new List<Airline>();
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            var airlineCreateViewModel = new AirlineCreateViewModel();
+            
+            return View(airlineCreateViewModel);
+        }
+
+        [AutoValidateAntiforgeryToken]
+        [HttpPost]
+        public IActionResult Create(AirlineCreateViewModel airlineViewModel)
+        {
+            var isValid = TryValidateModel(airlineViewModel);
+
+            if (isValid)
+            {
+                var newEntity = new Airline
+                {
+                    AirlineID = Guid.NewGuid(),
+                    Name = airlineViewModel.Name,
+                    CompanyNumber = airlineViewModel.CompanyNumber,
+                    CurrentPlanes = airlineViewModel.CurrentPlanes,
+                    Adress = airlineViewModel.Adress,
+                    Email = airlineViewModel.Email,
+                    PhoneNumber = airlineViewModel.PhoneNumber,                   
+                    AccountNumber = airlineViewModel.AccountNumber,
+                    //ListInvoices = airlineViewModel.ListInvoices,
+                    Logo = airlineViewModel.Logo,
+                };
+
+                airlineEntities.Add(newEntity);
+                //_myDatabase.AddAirline(newEntity);  
+                return RedirectToAction("Index");
+            }          
+            return View(airlineViewModel);
+        }
+    }
+}
---
+++ AirWaze/Controllers/PlaneController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AirWaze.Controllers
+{
+    public class PlaneController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
