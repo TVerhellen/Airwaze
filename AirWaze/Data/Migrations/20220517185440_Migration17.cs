80e7145147451f412b4f091b3796ee2bbc2f28fd
Harald_Vandoorne <harald.vandoorne@gmail.com>
2022-05-17 21:06:07 +0200

    Changes

---
+++ AirWaze/Data/Migrations/20220517185440_Migration17.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AirWaze.Data.Migrations
+{
+    public partial class Migration17 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
--- AirWaze/Entities/Airport.cs
+++ AirWaze/Entities/Airport.cs
@@ -1,6 +1,7 @@
 ﻿using AirWaze.Database;
 using AirWaze.Controllers;
 using AirWaze.Database.Design;
+using Microsoft.EntityFrameworkCore;
 
 namespace AirWaze.Entities
 {
@@ -10,58 +11,111 @@ namespace AirWaze.Entities
         private static string _name = "Batman Airport";
         private static string _adress = "Bosdreef 6 Istanbul Turkye";
         private static DateTime _currenttime = DateTime.Now;
+        private static Random generator = new Random();
+        public static IAirWazeDatabase myDatabase = HomeController._myDatabase;
+        public static bool IsOnline = false;
 
         public static Timer aTimer;
 
         public static string Name
         {
-            get { return _name; }           
-        }      
+            get { return _name; }
+        }
         public static string Adress
         {
-            get { return _adress; }            
+            get { return _adress; }
         }
-        public static DateTime CurrentTime 
-        { 
+        public static DateTime CurrentTime
+        {
             get { return _currenttime; }
-        }       
+        }
         public static List<Gate> Gates { get; set; }
 
         public static List<Flight> Flights { get; set; }
         public static List<Runway> Runways { get; set; }
 
-        public static List<Plane> Planes { get; set; }   
+        public static List<Plane> Planes { get; set; }
         public static Schedule CurrentSchedule { get; set; }
 
         public static Schedule ScheduleToApprove { get; set; }
 
         public static List<Schedule> ComingSchedules { get; set; }
-        
+
         public static void StartAirport()
         {
-            myDatabase = AirlineController._myDatabase;
-            Runways = myDatabase.GetRunways();
-            Gates = myDatabase.GetGates();
+            //Runways = myDatabase.GetRunways();
+            //foreach (Runway x in Runways)
+            //{
+            //    x.IsAvailable = true;
+            //}
+            //Gates = myDatabase.GetGates();
+            //foreach (Gate x in Gates)
+            //{
+            //    x.IsAvailable = true;
+            //}
+            //Flights = myDatabase.GetFlights();
+            //Planes = myDatabase.GetPlanes();
+            //Flights = Flights.FindAll(x => x.Status != 3 || x.Status != 5);
+            //Flights = (List<Flight>)Flights.OrderBy(flight => flight.Departure);
+            IsOnline = true;
+            //GenerateSchedule();
+            StartTimer(0);
+
+        }
+
+        public static void StartTimer(int dueTime)
+        {
+            aTimer = new Timer(new TimerCallback(TimerProc));
+            aTimer.Change(dueTime, 500);
+        }
+        private static void TimerProc(object state)
+        {
+            Timer t = (Timer)state;
+            t.Dispose();
+            UpdateAirport();
+        }
+        public static void UpdateAirport()
+        {
+            //foreach (Flight x in Flights)
+            //{
+            //    if (x.Departure.To <= _currenttime.Minute)
+            //    {
+            //        x.Status = 3;
+            //        myDatabase.UpdateFlight(x);
+            //        CurrentSchedule.Flights.Remove(x);
+            //    }
+            //    else if (x.Departure.Minute)
+            //}
+            //Flights = myDatabase.GetFlights();
+            //Planes = myDatabase.GetPlanes();
+            //Flights = Flights.FindAll(x => x.Status != 3 || x.Status != 5);
+            //Flights = (List<Flight>)Flights.OrderBy(flight => flight.Departure);
+            //if (CurrentSchedule.Flights.Count != 25)
+            //{
+            //    GenerateSchedule();
+            //}
         }
         public static void AddGate()
         {
             Gate thisgate = new Gate();
-            thisgate.GateID = Guid.NewGuid();
-            thisgate.
+            thisgate.GateID = Gates.Count + 1;
+            thisgate.Number = Gates.Count + 1;
+            myDatabase.AddGate(thisgate);
+            Gates.Add(thisgate);
         }
 
-        public static void AddRunways()
+        public static void AddRunway()
         {
             Runway thisRunway = new Runway();
-            thisRunway.RunwayID = Runways.Count +1;
-            thisRunway.Number = Runways.Count +1;
+            thisRunway.RunwayID = Runways.Count + 1;
+            thisRunway.Number = Runways.Count + 1;
             myDatabase.AddRunway(thisRunway);
             Runways.Add(thisRunway);
         }
 
         //Alle Shedule Crud -- Admin resticted
         public static Schedule GenerateSchedule()
-        {                    
+        {
             Schedule myshedule = new Schedule();
             myshedule.Date = _currenttime;
             myshedule.ScheduleID = generator.Next(0, 10000);
@@ -79,7 +133,7 @@ namespace AirWaze.Entities
                     theseflights[i].CurrentGate = Gates[i];
                     Gates[i].IsAvailable = false;
                     Gates[i].CurrentFlight = theseflights[i];
-                }               
+                }
             }
             for (int i = 0; i < Runways.Count; i++)
             {
@@ -114,12 +168,6 @@ namespace AirWaze.Entities
         public static void ViewSheduleAirliner()
         {
 
-        }
-
-        public static Schedule GenerateSchedule()
-        {
-            Schedule myshedule = new Schedule();
-            return myshedule;
         }
         public static Schedule ConfirmSchedule(Schedule thisschedule)
         {
