b9e7b3ae8739367f148597e38876c89eb3964a49
Harald_Vandoorne <harald.vandoorne@gmail.com>
2022-05-17 11:51:09 +0200

    AirportClasslogic

--- AirWaze/Entities/Airport.cs
+++ AirWaze/Entities/Airport.cs
@@ -10,6 +10,7 @@ namespace AirWaze.Entities
         private static string _name = "Batman Airport";
         private static string _adress = "Bosdreef 6 Istanbul Turkye";
         private static DateTime _currenttime = DateTime.Now;
+        private static Random generator = new Random();
         public static IAirWazeDatabase myDatabase;
 
         public static string Name
@@ -25,32 +26,87 @@ namespace AirWaze.Entities
             get { return _currenttime; }
         }       
         public static List<Gate> Gates { get; set; }
-        public static List<Runway> Runways { get; set; }   
+
+        public static List<Flight> Flights { get; set; }
+        public static List<Runway> Runways { get; set; }
+
+        public static List<Plane> Planes { get; set; }   
         public static Schedule CurrentSchedule { get; set; }
+
+        public static Schedule ScheduleToApprove { get; set; }
+
         public static List<Schedule> ComingSchedules { get; set; }
 
-        public static void StartUpAirport()
+        public static void UpdateAirport()
         {
             myDatabase = AirlineController._myDatabase;
-            Runways = myDatabase.GetRunways();
-            Gates = myDatabase.GetGates();
+            if (Runways == null)
+            {
+                Runways = myDatabase.GetRunways();
+                foreach(Runway x in Runways)
+                {
+                    x.IsAvailable = true;
+                }
+            }
+            if (Gates == null)
+            {
+                Gates = myDatabase.GetGates();
+                foreach(Gate x in Gates)
+                {
+                    x.IsAvailable = true;
+                }
+            }           
+            Flights = myDatabase.GetFlights();
+            Planes = myDatabase.GetPlanes();          
         }
         public static void AddGate()
         {
             Gate thisgate = new Gate();
-            thisgate.GateID = Guid.NewGuid();
-            thisgate.
+            thisgate.GateID = Gates.Count +1;
+            thisgate.Number = Gates.Count +1;
+            myDatabase.AddGate(thisgate);
+            Gates.Add(thisgate); 
         }
 
         public static void AddRunway()
         {
-            //ExtraRunwaysAanmaken
+            Runway thisRunway = new Runway();
+            thisRunway.RunwayID = Runways.Count +1;
+            thisRunway.Number = Runways.Count +1;
+            myDatabase.AddRunway(thisRunway);
+            Runways.Add(thisRunway);
         }
 
         //Alle Shedule Crud -- Admin resticted
-        public static void CreateShedule()
+        public static Schedule GenerateSchedule()
+        {                    
+            Schedule myshedule = new Schedule();
+            myshedule.Date = _currenttime;
+            myshedule.ScheduleID = generator.Next(0, 10000);
+            List<Flight> theseflights = new List<Flight>();
+            theseflights = (List<Flight>)theseflights.OrderBy(flight => flight.Departure);
+            for (int i = 0; i < 25; i++)
+            {
+                theseflights.Add(Flights[i]);
+            }
+            for (int i = 0; i < Gates.Count; i++)
+            {
+                theseflights[i].CurrentGate = Gates[i];
+                Gates[i].IsAvailable = false;
+                Gates[i].CurrentFlight = theseflights[i];
+            }
+            for (int i = 0; i < Runways.Count; i++)
+            {
+                theseflights[i].CurrentRunway = Runways[i];
+                Runways[i].IsAvailable = false;
+                Runways[i].CurrentFlight = theseflights[i];
+            }
+            return myshedule;
+        }
+
+        public static void FlightDeparts()
         {
-            //Shedules maken
+
         }
         public static void UpdateShedule()
         {
@@ -69,13 +125,7 @@ namespace AirWaze.Entities
         public static void ViewSheduleAirliner()
         {
 
-        }
-
-        public static Schedule GenerateSchedule()
-        {
-            Schedule myshedule = new Schedule();
-            return myshedule;
-        }
+        }      
         public static Schedule ConfirmSchedule(Schedule thisschedule)
         {
             return thisschedule;
