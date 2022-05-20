3a1741abc2ff86722e5e03b6e6f86ea371b8da39
Bart_VW <bart.vanw@gmail.com>
2022-05-20 10:09:06 +0200

    fixed createFlight

--- AirWaze/Database/AirWazeDatabase.cs
+++ AirWaze/Database/AirWazeDatabase.cs
@@ -24,9 +24,10 @@ namespace AirWaze.Database
         {
             _dbContext.Flights.Add(flight);
             _dbContext.Entry(flight.CurrentPlane).State = EntityState.Unchanged;
+            _dbContext.Entry(flight.CurrentPlane.CurrentAirline).State = EntityState.Unchanged;
             //_dbContext.Entry(flight.CurrentGate).State = EntityState.Unchanged;
             //_dbContext.Entry(flight.CurrentRunway).State = EntityState.Unchanged;
-            //_dbContext.SaveChanges();
+            _dbContext.SaveChanges();
         }
 
         public void AddPlane(Plane plane)
