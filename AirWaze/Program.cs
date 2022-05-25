54557c462adf2fba8db547046a92386c2784a79a
Harald_Vandoorne <harald.vandoorne@gmail.com>
2022-05-25 07:43:01 +0200

    ooops

--- AirWaze/Program.cs
+++ AirWaze/Program.cs
@@ -79,7 +79,7 @@ app.UseSession();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Airline}/{action=List}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.MapRazorPages();
 app.MapBlazorHub();
